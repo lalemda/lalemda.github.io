e1a976f989fe9e65dbb83fce320189d875d13e91
lalemda <151401955+lalemda@users.noreply.github.com>
2023-11-22 22:10:17 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -214,8 +214,8 @@
           let storedPassword = data.password;
           if (passwordInputValue === storedPassword) {
             localStorage.setItem("checkPass", passwordInputValue);
-            pass_container.classList.add('hide-pass');
             window.location.reload();
+            pass_container.classList.add('hide-pass');
             // showMessage("Password is correct!");
           } else {
             showMessage("Password is incorrect. Please try again.");
